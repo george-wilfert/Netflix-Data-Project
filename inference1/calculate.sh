e7306c301b58b33774e6e2a5ed6647a651738088
George Wilfert <151199530+george-wilfert@users.noreply.github.com>
2024-05-10 17:24:35 -0700

    Update calculate.sh

--- inference1/calculate.sh
+++ inference1/calculate.sh
@@ -1,1 +1,9 @@
+#!/bin/bash
+
+for i in {2008..2021}; do 
+	sameyear=$(grep $i sortedDifferences.txt | awk -F ":" 'BEGIN {samecount=0} {if($2==0) samecount++} END {print samecount}');
+	total=$(grep $i sortedDifferences.txt | wc -l);	
+	dif=$((total-sameyear));
+	echo $i,$sameyear,$dif;
+done
 
